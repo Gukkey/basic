252eea9ab549b7f99a3f2778f1df7404191432fe
Gukkey <gukkey@protonmail.com>
2025-08-02 20:21:49 +0530

    refactor: remove hardcoded MongoDB connection string and related comments

--- index.js
+++ index.js
@@ -10,22 +10,6 @@ app.use(express.json());
 
 app.use("/api/diaries", DiaryRoute);
 
-// mongoose
-//   .connect(
-//     `mongodb+srv://guhantamilvanan:aZQobScuPTl0Vntb@basiccluster.ravt0mm.mongodb.net/basic?retryWrites=true&w=majority&appName=BasicCluster`
-//   )
-//   .then(() => {
-//     console.log("connected to database");
-
-//     // this piece of code will start listening from port 3000, if changed to 8000 it will be listening to 8000
-//     app.listen(3000, () => {
-//       console.log("listening on port 3000");
-//     });
-//   })
-//   .catch(() => {
-//     console.log("could not connect to db");
-//   });
-
 mongoose
   .connect(process.env.MONGO_DB_CONNECTION_STRING)
   .then(() => {
